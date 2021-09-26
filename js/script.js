226f21960e4dc056b2f6a2589dac213335401bad
Shivanand <mpshivanandpatil2002@gmail.com>
2021-09-26 14:44:35 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var link1 = 'https://bit.ly/3k5lCeE';
+var link2 = 'https://bit.ly/3z7hOxF';
+var link3 = 'https://bit.ly/3tzO9w5';
+var link4 = 'https://bit.ly/3A2JLrP';
+var link5 = 'https://bit.ly/3tE1lzP';
+
+
+
+function copyLink1() {
+    navigator.clipboard.writeText(link1);
+    alert("Copied link succesfully");
+}
+
+function copyLink2() {
+    navigator.clipboard.writeText(link2);
+    alert("Copied link succesfully");
+}
+
+function copyLink3() {
+    navigator.clipboard.writeText(link3);
+    alert("Copied link succesfully");
+}
+
+function copyLink4() {
+    navigator.clipboard.writeText(link4);
+    alert("Copied link succesfully");
+}
+
+function copyLink5() {
+    navigator.clipboard.writeText(link5);
+    alert("Copied link succesfully");
+}
+
