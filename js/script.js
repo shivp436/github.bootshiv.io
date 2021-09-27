4cc9039ecd8de7c3d96509c8cf9415f2ee6c16c1
Shivanand <mpshivanandpatil2002@gmail.com>
2021-09-28 01:07:48 +0530

    Added linkCopiedModal

--- js/script.js
+++ js/script.js
@@ -8,26 +8,26 @@ var link5 = 'https://bit.ly/3tE1lzP';
 
 function copyLink1() {
     navigator.clipboard.writeText(link1);
-    alert("Copied link succesfully");
+//    alert("Copied link succesfully");
 }
 
 function copyLink2() {
     navigator.clipboard.writeText(link2);
-    alert("Copied link succesfully");
+//    alert("Copied link succesfully");
 }
 
 function copyLink3() {
     navigator.clipboard.writeText(link3);
-    alert("Copied link succesfully");
+//    alert("Copied link succesfully");
 }
 
 function copyLink4() {
     navigator.clipboard.writeText(link4);
-    alert("Copied link succesfully");
+//    alert("Copied link succesfully");
 }
 
 function copyLink5() {
     navigator.clipboard.writeText(link5);
-    alert("Copied link succesfully");
+//    alert("Copied link succesfully");
 }
 
